fc52967b477c3a4fb60a62b7da7f31e8381882e3
Aiman Misbah <aimanmisbah06@gmail.com>
2025-03-07 20:05:16 +0500

    Update 3.cpp

--- Lab 05/3.cpp
+++ Lab 05/3.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class Employee;
+
+class Project
+{
+private:
+    string title;
+    string deadline;
+    Employee *employees[3];
+    int countEmployees = 0;
+
+public:
+    Project(string title, string deadline) : title(title), deadline(deadline) {}
+
+    void addEmployee(Employee *employee);
+
+    void displayProjectDetails()
+    {
+        cout << "Title: " << title << endl;
+        cout << "Deadline: " << deadline << endl;
+    }
+
+    void displayAllDetails();
+};
+
 class Employee
 {
 private:
@@ -11,65 +35,81 @@ private:
 
 public:
     Employee(string name, string designation) : name(name), designation(designation) {}
+
     void displayEmployeeDetails()
     {
-        cout << "Name: " << endl;
-        cout << "Designation: " << endl
-             << endl;
+        cout << "Name: " << name << endl;
+        cout << "Designation: " << designation << endl;
     }
-    void addProjects(Project *p)
+
+    void addProject(Project *project)
     {
-        if(projectCount>0)
         if (projectCount < 3)
         {
-            projects[projectCount++] = p;
+            projects[projectCount++] = project;
         }
         else
         {
-            cout << "Projects Capacity Full, Cannot Add More Projects";
-        }
-        else{
-            cout<<"Invalid Project Number! "<<endl;
+            cout << "Cannot Add More Projects, Limit Reached!" << endl;
         }
     }
-    void displayProjectDetails(){
-        for(int i=0;i<projectCount;i++){
-            projects[i].displayProjectDetails();
-        }
-    }
-};
 
-class Project
-{
-private:
-    string title;
-    string deadline;
-    int countEmployees = 0;
-    Employee *employees[3];
-
-public:
-    Project(string title, string deadline) : title(title), deadline(deadline) {}
-    void addEmployee(Employee *e)
+    void displayProjectDetails()
     {
-        employees[countEmployees++] = e;
-    }
-    void displayProjectDetails(){
-
-    }
-    void displayAllDetails()
-    {
-        cout << "----------Displaying Project Details---------" << endl;
-        cout << "Title: " << title << endl;
-        cout << "Deadline: " << deadline << endl;
-        cout << "Number Of Employees: " << countEmployees << endl;
-        for (int i = 0; i < countEmployees; i++)
+        cout << "Employee " << name << " is working on the following projects: " << endl;
+        for (int i = 0; i < projectCount; i++)
         {
-            employees[i]->displayEmployeeDetails();
+            projects[i]->displayProjectDetails();
         }
     }
 };
+
+void Project::addEmployee(Employee *employee)
+{
+    if (countEmployees < 3)
+    {
+        employees[countEmployees++] = employee;
+        employee->addProject(this);
+    }
+    else
+    {
+        cout << "Cannot Add More Employees, Limit Reached!" << endl;
+    }
+}
+
+void Project::displayAllDetails()
+{
+    displayProjectDetails();
+    cout << "Number Of Employees: " << countEmployees << endl;
+    for (int i = 0; i < countEmployees; i++)
+    {
+        employees[i]->displayEmployeeDetails();
+    }
+}
 
 int main()
 {
+    Employee employee1("Aiman", "Game Developer");
+    Employee employee2("Urooj", "Data Analyst");
+
+    Project project1("Web Development", "30 June 2025");
+    Project project2("AI Development", "30 July 2025");
+
+    project1.addEmployee(&employee1);
+    project1.addEmployee(&employee2);
+    project2.addEmployee(&employee1);
+
+    cout << "----------Displaying Details Of All Projects-----------" << endl;
+    project1.displayAllDetails();
+    cout << endl;
+    project2.displayAllDetails();
+    cout << endl;
+
+    cout << "----------Displaying Details Of All Employees-----------" << endl;
+    employee1.displayProjectDetails();
+    cout << endl;
+    employee2.displayProjectDetails();
+    cout << endl;
+
     return 0;
 }

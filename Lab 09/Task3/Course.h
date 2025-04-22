0ea5303b3b686db21582cea5be712224752594a0
Aiman Misbah <aimanmisbah06@gmail.com>
2025-04-22 21:57:39 +0500

    Update Course.h

--- Lab 09/Task3/Course.h
+++ Lab 09/Task3/Course.h
@@ -1,1 +1,21 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <string>
+using namespace std;
+
+class Course
+{
+protected:
+    string courseCode;
+    int credits;
+
+public:
+    Course(string code, int c) : courseCode(code), credits(c) {}
+    virtual void calculateGrade() = 0; // Pure virtual function
+    virtual void displayInfo() = 0;    // Pure virtual function
+    virtual ~Course() {}
+};
+
+#endif
 

a9c6cdcaadc683c89f55c90be8168aa6dfa22948
Aiman Misbah <aimanmisbah06@gmail.com>
2025-05-04 21:15:28 +0500

    Update 1.cpp

--- Lab 10/1.cpp
+++ Lab 10/1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class VehicleReader {
+public:
+    void processFile(const string& fileName) {
+        ifstream source(fileName);
+        string entry;
+
+        if (!source) {
+            cout << "Unable to access " << fileName << endl;
+            return;
+        }
+
+        while (getline(source, entry)) {
+            if (entry.empty() || entry.front() == '#') {
+                continue;
+            }
+            cout << entry << endl;
+        }
+
+        source.close();
+    }
+};
+
+int main() {
+    VehicleReader loader;
+    loader.processFile("vehicles.txt");
+    return 0;
+}
 
